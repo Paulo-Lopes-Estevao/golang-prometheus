3a9df47d26e6610440c97f8555140b677f00e44a
Paulo-Lopes-Estevao <pl1745240@gmail.com>
2021-11-18 14:45:46 +0100

    edit: add prometheus config metrics

--- go.mod
+++ go.mod
@@ -3,8 +3,6 @@ module github.com/Paulo-Lopes-Estevao/prometheus_gopher
 go 1.16
 
 require (
-	github.com/labstack/echo v3.3.10+incompatible
-	github.com/labstack/gommon v0.3.1 // indirect
-	github.com/prometheus/client_golang v1.11.0
-	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871 // indirect
+	github.com/labstack/echo-contrib v0.11.0
+	github.com/labstack/echo/v4 v4.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,22 +3,22 @@ package main
 import (
 	"log"
 
-	"github.com/labstack/echo"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/labstack/echo-contrib/prometheus"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	route := echo.New()
+	p := prometheus.NewPrometheus("echo", nil)
 
-	route.GET("/", Home)
-	route.GET("/metrics", echo.WrapHandler(promhttp.Handler()))
+	route.GET("/", func(c echo.Context) error {
+		return c.JSON(200, "Hello world!")
+	})
 
-	if err := route.Start(":4000"); err != nil {
+	p.Use(route)
+
+	if err := route.Start(":9000"); err != nil {
 		log.Println("Not Running Server A...", err.Error())
 	}
 
-}
-
-func Home(c echo.Context) error {
-	return c.JSON(200, "Certo")
 }
