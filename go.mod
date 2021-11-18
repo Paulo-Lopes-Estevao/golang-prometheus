d3c9cb6c37ae5bb541de6fe9d9da87ec1a6cfaac
Paulo-Lopes-Estevao <pl1745240@gmail.com>
2021-11-18 09:49:13 +0100

    add: prometheus

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/prometheus/client_golang v1.11.0
 	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871 // indirect
 )
--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"log"
 
 	"github.com/labstack/echo"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 	route := echo.New()
 
 	route.GET("/", Home)
+	route.GET("/metrics", echo.WrapHandler(promhttp.Handler()))
 
 	if err := route.Start(":4000"); err != nil {
 		log.Println("Not Running Server A...", err.Error())
